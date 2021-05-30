16f3c2601537d3bfbe9766dac7ced5ed72fb01f6
mkcarl <53103933+mkcarl@users.noreply.github.com>
2021-05-30 14:13:23 +0800

    done projectPeople.lead

--- intents.py
+++ intents.py
@@ -9,6 +9,7 @@ graph = Graph(
     password="4Pd1FlT3AOHxIlf6AoUKQYN09ajnrBOD7_l7mzO0tow"
     )
 
+temp = dict()
 
 def return_fulfillment(data): 
     intent = data["queryResult"]["intent"]["displayName"]
@@ -65,6 +66,17 @@ def return_fulfillment(data):
             f"Yes, {comp['CompanyName']} is located in {region}", 
             f"Yup, {comp['CompanyName']} is from {region} "
         ]
+    elif intent =="ProjectPeople.Lead": 
+        # who is the techlead of Muscle Fitness management system
+        project_name = data["queryResult"]["parameters"]["system-name"]
+        cypher_query = f"match (proj:Project)-[:LEAD_BY]->(e:Employee) where proj.ProjectName =~ '(?i){project_name}' RETURN e"
+        all_leader = graph.run(cypher_query).data()
+        leader = random.choice(all_leader)["e"]
+        choices = [
+            f"The tech lead of the project {project_name} is {leader['EmployeeName']}", 
+            f"{leader['EmployeeName']} leaded the project {project_name}"
+        ]
+ 
     elif intent == "ProjectSkill.All": 
         # What is used in Berjaya fitness system? 
         project_name = data["queryResult"]["parameters"]["project-name"]
@@ -80,23 +92,79 @@ def return_fulfillment(data):
     elif intent == "help": 
         # if the user needs help 
         query_examples = [
-            "Who knows java?",  
-            "What projects did we do in the past involved C?",  
-            "Do you know who is the tech lead of our las project with Singtel?",  
-            "What is used in Berjaya Fitness System?", 
-            "Did we had any affiliations with a company in Thailand?"
-        ]    
+            "Who knows java?",  # works
+            "What projects did we do in the past involved C?", # works  
+            "Do you know who is the tech lead of our last project with Singtel?", # works  
+            "What is the skills used in Muscle Fitness management system?", # works
+            "Did we had any clients from Thailand?" # works
+        ]  
+        new_data_examples = [
+            "Add employee name John Cena", # works 
+            "Project CNNB by Big Show is lead by GotLightE", 
+            "Add new skill Java", # works
+            "Make new data for Philip's in Electronics industry in Netherlands" # works
+        ]  
 
         final_output =  f"These are some examples of what you can do : "\
-                        f"\n - {random.choice(query_examples)}"\
-                        f"\n - {random.choice(query_examples)}"\
-                        f"\n - {random.choice(query_examples)}"\
+                        f"\n - {query_examples.pop(random.randint(0, len(query_examples)))}"\
+                        f"\n - {random.choice(new_data_examples)}"\
+                        f"\n - {query_examples.pop(random.randint(0, len(query_examples) - 1))}"\
         
         choices = [final_output]
     elif intent == "NewData.Project": 
-        new_project_name = ""
-        cypher_query = f"MATCH ()"
 
+        leader_f = data["queryResult"]["parameters"]["leader_fname"]
+        leader_l = data["queryResult"]["parameters"]["leader_lname"]
+        leader_name = f"{leader_f} {leader_l}"
+        company_name = data["queryResult"]["parameters"]["compName"][0]
+        project_name = data["queryResult"]["parameters"]["projName"]
+
+        temp["leader_name"] = leader_name
+        temp["company_name"] = company_name
+        temp["project_name"] = project_name
+
+        return json.dumps(data)
+
+    elif intent == "NewData.Project - contributer - skill": # as of now, doesnt work
+
+        skills: list = data["queryResult"]["parameters"]["skill"]
+        names_matrix = [
+            data["queryResult"]["outputContexts"][0]["parameters"]["fname"], 
+            data["queryResult"]["outputContexts"][0]["parameters"]["lname"]
+        ]
+        names_tuple = list(zip(*names_matrix))
+        names = list(f"{name[0]} {name[1]}" for name in names_tuple)
+        
+        cypher_query = f"CREATE (p:Project {{ ProjectName: '{temp['project_name']}' }}) "\
+                        f"WITH p "\
+                        f"MATCH (e:Employee) "\
+                        f"WHERE (e.EmployeeName =~ '{temp['leader_name']}') "\
+                        f"MERGE (p)-[l:LEADS_BY]->(e) "\
+                        f""\
+                        f"WITH p MATCH (c:Company) "\
+                        f"WHERE (c.CompanyName =~ '{temp['company_name']}') "\
+                        f"MERGE (c)-[o:ORGANISE]->(p) "\
+                        f""\
+                        f"CREATE (n:N {{skill:'{','.join(skills)}'}}) "\
+                        f"WITH n "\
+                        f"MATCH (p:Project {{ProjectName:'{temp['project_name']}'}}) "\
+                        f"UNWIND split(n.skill, ',') AS SKILL "\
+                        f"MERGE (s:Skill {{skill: SKILL}}) "\
+                        f"MERGE (p)-[r:REQUIRES]->(s) "\
+                        f""\
+                        f"CREATE (m:M {{emp:'{','.join(names)}'}}) "\
+                        f"WITH m "\
+                        f"MATCH (p:Project {{ProjectName:'{temp['project_name']}'}}) "\
+                        f"UNWIND split(m.emp, ',') AS EMP "\
+                        f"MERGE (e:Employee {{EmployeeName: EMP}}) "\
+                        f"MERGE (e)-[i:INVOLVE_IN]->(p) "\
+                        f"DELETE n, m"
+        
+        graph.run(cypher_query)
+
+        output = [
+            "Success!"
+        ]
     elif intent == "NewData.Company": 
         new_company_name = data["queryResult"]["parameters"]["compName"]
         new_company_industry = data["queryResult"]["parameters"]["industry"]
@@ -133,15 +201,23 @@ def return_fulfillment(data):
                 f"Skill already exist!", 
                 f"Existing skills cannot be added again. :("
             ] 
-    elif intent == "NewData.Employee": 
-        first_name = data["queryResult"]["parameters"]["emp_fame"]
-        last_name = data["queryResult"]["parameters"]["emp_lname"]
+
+    elif intent == "NewData.Employee - skill": 
+        first_name = data["queryResult"]["outputContexts"][0]["parameters"]["emp_fame"]
+        last_name = data["queryResult"]["outputContexts"][0]["parameters"]["emp_lname"]
         new_employee_name = f"{first_name} {last_name}"
+        skills:list = data["queryResult"]["parameters"]["skill"]
         cypher_query_check = f"MATCH (e:Employee) WHERE e.EmployeeName =~ '(?i){new_employee_name}' RETURN e"
         exist = len(list(graph.run(cypher_query_check))) > 0
 
         if not exist: 
-            cypher_query = f"CREATE (e:Employee {{ EmployeeName: '{new_employee_name}' }})"
+            cypher_query = f"CREATE (e:Employee {{EmployeeName:'{new_employee_name}'}}) "\
+                            f"CREATE (n:N {{skill:'{','.join(skills)}'}}) "\
+                            f"WITH n "\
+                            f"UNWIND split(n.skill, ',') AS SKILL "\
+                            f"MERGE (s:Skill {{skill: SKILL}}) "\
+                            f"MERGE (e)-[:POSSESS]->(s) "\
+                            f"DELETE n"
             graph.run(cypher_query)
             choices = [
                 f"{new_employee_name} has joined your party!", 
@@ -152,7 +228,7 @@ def return_fulfillment(data):
                 f"Warning! {new_employee_name} is already in the party!", 
                 f"Employee was already in the team..."
             ]
-    elif intent == "testCardResponse": 
+    elif intent == "testCardResponse": # only for testing purposes
         custom_card = {
                         "fulfillmentMessages": [
                             {
@@ -171,6 +247,7 @@ def return_fulfillment(data):
                         ]
                         }
         return json.dumps(custom_card)
+
     else: 
         return data
     
@@ -181,5 +258,3 @@ def return_fulfillment(data):
             'source' : 'webhookdata'
         }
         return json.dumps(output)
-
-# TODO : ProjectPeople.Lead intent 
